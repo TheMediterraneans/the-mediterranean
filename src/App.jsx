565f703f3de44f94ace7c9edbe8b5778f4da3bb4
Zefi Kavvadia <zefi.kavvadia@di.huc.knaw.nl>
2025-07-18 17:36:52 +0200

    test installation of DaisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,23 @@ function App() {
   return (
     <>
 
-      <h1>Vite + React</h1>
+    <div className="p-6">
+      <button className="btn btn-primary">
+        ✅ DaisyUI is working!
+      </button>
+    </div>
+
+    <div className="card w-96 bg-base-100 shadow-xl">
+  <figure>
+    <img src="https://placekitten.com/200/200" alt="a card"/>
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Just a card</h2>
+    <p>If you can see this card, DaisyUI works!</p>
+    <button className="btn btn-secondary">Click me</button>
+  </div>
+</div>
+
 
     </>
   )
