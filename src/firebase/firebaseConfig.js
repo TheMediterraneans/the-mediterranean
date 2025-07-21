c9b594385611d7817083bee5d4e66a0dddba92df
Luana Ruscitti <ruscittiluanagmail.com>
2025-07-21 11:42:22 +0200

    firebase config file added

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "src/firebase/app";
+import { getAuth } from "src/firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAF7FS4wjDIiv0dDpUU2mF4JJS0ue5MDxE",
+  authDomain: "the-mediterraneans.firebaseapp.com",
+  databaseURL: "https://the-mediterraneans-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "the-mediterraneans",
+  storageBucket: "the-mediterraneans.firebasestorage.app",
+  messagingSenderId: "29302074855",
+  appId: "1:29302074855:web:6d657656f2c82354abe142"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
